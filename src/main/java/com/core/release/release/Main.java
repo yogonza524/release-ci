c559abff5dbcb95261320f8ad27f7d93505fd862
Gonzalo Mendoza <yogonza524@gmail.com>
2017-06-14 18:23:21 -0300

    Testing

--- src/main/java/com/core/release/release/Main.java
+++ src/main/java/com/core/release/release/Main.java
@@ -15,6 +15,6 @@ package com.core.release.release;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Geniaal");
     }
 }
