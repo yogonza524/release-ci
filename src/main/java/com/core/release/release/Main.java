4232a674ffb61c5ea673c13cac71eb2686ffef93
Gonzalo Mendoza <yogonza524@gmail.com>
2017-06-15 14:29:08 -0300

    Testing

--- src/main/java/com/core/release/release/Main.java
+++ src/main/java/com/core/release/release/Main.java
@@ -16,6 +16,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Geniaal");
-//        System.out.println("Buenisimo");
+        System.out.println("Buenisimo");
     }
 }
